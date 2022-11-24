11152b7ee23e3ae8d82f2b997707bb0dd0c5db72
Christian Hinkle <c4hinkle@gmail.com>
2022-11-23 23:40:17 -0500

    Upgrade to UE 5.1 and update plugins.
    Update config to enable SM6. Remove AndroidFileServer section of config.
    
    Use EngineIncludeOrderVersion.Latest in .Target.cs files.
    
    Update plugins to work for UE 5.1 and update them to latest commits.

--- _GameProjectName_/Source/_GameProjectName_.Target.cs
+++ _GameProjectName_/Source/_GameProjectName_.Target.cs
@@ -9,6 +9,7 @@ public class _GameProjectName_Target : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.AddRange(new string[] { "_GameProjectName_" });
 	}
 }
